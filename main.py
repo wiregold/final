bcf40419468934f67a28f3c2ccf90b72ff7d4705
wiregold <73246580+wiregold@users.noreply.github.com>
2020-10-21 18:59:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,40 @@ def caesar(plaintext, shift):
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     table = str.maketrans(alphabet, shifted_alphabet)
     return plaintext.translate(table)
+
+
+
+
+@app.route('/substitution', methods=['GET', 'POST'])
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz.,! '
+
+def makeKey(alphabet):
+    alphabet = list(alphabet)
+    random.shuffle(alphabet)
+    return ''.join(alphabet)
+
+
+key = makeKey(alphabet)
+plaintext = "Hey, this is really fun!"
+
+
+# v! zmhvxdmxdmo!nll mikbg
+
+
+def encrypt(plaintext, key, alphabet):
+    keyIndices = [alphabet.index(k.lower()) for k in plaintext]
+    return ''.join(key[keyIndex] for keyIndex in keyIndices)
+
+
+def decrypt(cipher, key, alphabet):
+    keyIndices = [key.index(k) for k in cipher]
+    return ''.join(alphabet[keyIndex] for keyIndex in keyIndices)
+
+
+cipher = encrypt(plaintext, key, alphabet)
+
+print(plaintext)
+print(cipher)
+print(key)
+print(decrypt(cipher, key, alphabet))
