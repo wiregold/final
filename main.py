8b2927a41f14137370a0439a67f36f1cab3a9c72
wiregold <73246580+wiregold@users.noreply.github.com>
2020-10-21 13:57:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        language = request.form.get('language')
+        framework = request.form['framework']
+
+        return '''<h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
     return '''<form method="POST">
                   Language: <input type="text" name="language"><br>
                   Framework: <input type="text" name="framework"><br>
