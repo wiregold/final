0ffc5402a34c721dd15dd7b6ce46e777d4346ea7
wiregold <73246580+wiregold@users.noreply.github.com>
2020-10-21 14:06:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def encrypt_example():
         return '''<h1>ur ciphertext is{}</h1>'''.format(cipherText)
 
     return '''<form method="POST">
-                  Language: <input type="text" name="language"><br>
-                  Framework: <input type="text" name="framework"><br>
+                  Language: <input type="text" name="plainText"><br>
+                  Framework: <input type="text" name="shift"><br>
                   <input type="submit" value="Submit"><br>
               </form>'''
 
@@ -31,3 +31,46 @@ def encrypt():
     shift = int(input("What is your shift?"))
     cipherText = caesar(plainText, shift)
     print("Your ciphertext is: ", cipherText, "with a shift of", shift)
+
+"""
+def decrpyt():
+    encryption = input("Enter in your encrypted code")
+
+    n = int(input("enter in your encryption shift"))
+    encryption_shift = n * -1
+    cipherText1 = ""
+    cipherText1 = caesar(encryption, encryption_shift)
+    print("Your ciphertext is: ", cipherText1, "with a  negative shift of", encryption_shift)
+
+
+def caesar(plaintext, shift):
+    alphabet = string.ascii_lowercase
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return plaintext.translate(table)
+
+
+def helloword():
+    print("Hello World!")
+
+
+from tkinter import *
+
+menu = Tk()
+menu.title("menu")
+menu.geometry("300x300")
+
+button0 = Button(menu, text="Hello World", command=helloword)
+button0.pack()
+
+button1 = Button(menu, text="Encrypt", command=encrypt)
+button1.pack()
+
+button2 = Button(menu, text="Decrypt", command=decrpyt)
+button2.pack()
+
+button3 = Button(menu, text="Exit", command=exit)
+button3.pack()
+
+menu.mainloop()
+"""
