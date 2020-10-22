b97c36d13c8e4c85d6b4246246d24c63c6e122cc
wiregold <73246580+wiregold@users.noreply.github.com>
2020-10-22 00:28:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def encrypt_example():
             shift1 = var3 * -1
             cipherText1 = caesar(plainText1, shift1)
             return '''<h1>The Unencrypted value is:{}</h1>
-                                      <h1>The shift value is:{}</h1>'''.format(cipherText1, shift1)
+                    <h1>The shift value is:{}</h1>'''.format(cipherText1, shift1)
 
         elif request.form['submit_button'] == 'Submit Substitution':
 
@@ -60,33 +60,86 @@ def encrypt_example():
             sub2 = decrypt(cipher2, key2, alphabet2)
             return '''<h1>The Unencrypted value is:{}</h1>'''.format(sub2)
 
-    return '''<form method="POST">
+    return '''
+    
+    
+    <form method="POST">
+                  <html>
+                  <head>
+                  </head>                  
+                  <style>
+h1   {color: Grey;
+        font-family: Verdana}
+
+input[type=text], select {
+  width: 40%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 50%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+label{
+    display: inline-block;
+    font-family: Verdana;
+    font-size: 17;
+    width: 10%;
+    text-align: left;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+</style>
+                  <body>
                   
                   <h1>Caesar Encryption</h1>  
-                  Insert Plaintext: <input type="text" name="plainText"><br>
-                  Insert Shift Value: <input type="text" name="var1"><br>
+                  <label for="plainText">Insert Plaintext:</label> <input type="text" name="plainText"><br>
+                  <label for="var1">Insert Shift Value:</label> <input type="text" name="var1"><br>
                   <input type="submit" name= "submit_button" value="SubmitEncrypt"><br>
                   <br><br><br>
                   
                   <h1>Caesar Decryption</h1>  
-                  Insert Encrypted Text: <input type="text" name="plainText1"><br>
-                  Insert Shift Value: <input type="text" name="var2"><br>
+                 <label for="plainText1"> Insert Encrypted Text:</label> <input type="text" name="plainText1"><br>
+                  <label for="var2">Insert Shift Value:</label> <input type="text" name="var2"><br>
                   <input type="submit" name= "submit_button" value="SubmitDecrypt"><br>
                   <br><br><br>
                   
+                  <hr noshade width=100% align=left>
                   
                   <h1>Substitution Encryption</h1>  
-                  Insert Plaintext: <input type="text" name="subtext1"><br>
+                  <label for="subtext1">Insert Plaintext:</label> <input type="text" name="subtext1"><br>
                   <input type="submit" name= "submit_button" value="Submit Substitution"><br>
                   <br><br><br>
                   
                   <h1>Substitution Decryption</h1>  
-                  Insert Encrypted Text: <input type="text" name="subtext2"><br>
-                  Insert Key: <input type="text" name="key2"><br>
+                  <label for="subtext2">Insert Encrypted Text:</label> <input type="text" name="subtext2"><br>
+                  <label for="key2">Insert Key:</label> <input type="text" name="key2"><br>
                   <input type="submit" name= "submit_button" value="Submit Substitution Decrypt"><br>
                   <br><br><br>
-
-              </form>'''
+    </body>
+              </form>
+              </html>
+              '''
 
 
 def caesar(plaintext, shift):
