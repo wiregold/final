b8a8ce6466d5fd0d4fc3951fe369d47aad55dfd0
wiregold <73246580+wiregold@users.noreply.github.com>
2020-11-23 11:53:15 -0500

    Update camera_opencv.py

--- camera_opencv.py
+++ camera_opencv.py
@@ -4,7 +4,7 @@ from base_camera import BaseCamera
 
     
 class Camera(BaseCamera):
-    video_source =  '4a604b20d6ac.ngrok.io'
+    video_source = '4a604b20d6ac.ngrok.io'
 
     def __init__(self):
         if os.environ.get('OPENCV_CAMERA_SOURCE'):
