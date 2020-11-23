4e01937a10336c79262e5aa7e4f5fb5765f3714f
wiregold <73246580+wiregold@users.noreply.github.com>
2020-11-23 02:41:46 -0500

    Update camera_opencv.py

--- camera_opencv.py
+++ camera_opencv.py
@@ -4,7 +4,7 @@ from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
-    video_source = -1
+    video_source = 0
 
     def __init__(self):
         if os.environ.get('OPENCV_CAMERA_SOURCE'):
