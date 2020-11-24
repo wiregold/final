51c2cbc4f73b3b62de8ba772436f2e0809adffab
wiregold <73246580+wiregold@users.noreply.github.com>
2020-11-24 10:50:17 -0500

    Update base_camera.py

--- base_camera.py
+++ base_camera.py
@@ -1,89 +1,104 @@
-from importlib import import_module
-import os
-from flask import Flask, redirect, url_for, session, render_template, Response
-from authlib.integrations.flask_client import OAuth
-from auth_decorator import login_required
-from camera_opencv import Camera
-import cv2
+import time
+import threading
+try:
+    from greenlet import getcurrent as get_ident
+except ImportError:
+    try:
+        from thread import get_ident
+    except ImportError:
+        from _thread import get_ident
 
 
-app = Flask(__name__)
+class CameraEvent(object):
+    """An Event-like class that signals all active clients when a new frame is
+    available.
+    """
+    def __init__(self):
+        self.events = {}
 
-app.secret_key = 'random secret'
-oauth = OAuth(app)
-video = cv2.VideoCapture(0)
+    def wait(self):
+        """Invoked from each client's thread to wait for the next frame."""
+        ident = get_ident()
+        if ident not in self.events:
+            # this is a new client
+            # add an entry for it in the self.events dict
+            # each entry has two elements, a threading.Event() and a timestamp
+            self.events[ident] = [threading.Event(), time.time()]
+        return self.events[ident][0].wait()
 
-google = oauth.register(
-    name='google',
-    client_id='303773199618-f4b58nlg6migc21qii5pqm2m3av07qro.apps.googleusercontent.com',
-    client_secret='IrRzNZI1Fr3Y3xSDrjG5_zaZ',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    client_kwargs={'scope': 'openid email profile'},
-)
+    def set(self):
+        """Invoked by the camera thread when a new frame is available."""
+        now = time.time()
+        remove = None
+        for ident, event in self.events.items():
+            if not event[0].isSet():
+                # if this client's event is not set, then set it
+                # also update the last set timestamp to now
+                event[0].set()
+                event[1] = now
+            else:
+                # if the client's event is already set, it means the client
+                # did not process a previous frame
+                # if the event stays set for more than 5 seconds, then assume
+                # the client is gone and remove it
+                if now - event[1] > 5:
+                    remove = ident
+        if remove:
+            del self.events[remove]
 
-
-@app.route('/')
-def root():
-    return render_template('login.html')
-
-
-def gen(camera):
-    while True:
-        while True:
-            frame = camera.get_frame()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/stream')
-def stream():
-    return render_template('stream.html')
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    def clear(self):
+        """Invoked from each client's thread after a frame was processed."""
+        self.events[get_ident()][0].clear()
 
 
-@app.route('/video')
-def vid():
-    return render_template('video.html')
+class BaseCamera(object):
+    thread = None  # background thread that reads frames from camera
+    frame = None  # current frame is stored here by background thread
+    last_access = 0  # time of last client access to the camera
+    event = CameraEvent()
 
+    def __init__(self):
+        """Start the background camera thread if it isn't running yet."""
+        if BaseCamera.thread is None:
+            BaseCamera.last_access = time.time()
 
-@app.route('/login')
-def login():
-    session['logged_in'] = True
-    google = oauth.create_client('google')
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
+            # start background frame thread
+            BaseCamera.thread = threading.Thread(target=self._thread)
+            BaseCamera.thread.start()
 
+            # wait until frames are available
+            while self.get_frame() is None:
+                time.sleep(0)
 
-@app.route('/authorize')
-def authorize():
-    google = oauth.create_client('google')
-    token = google.authorize_access_token()
-    resp = google.get('userinfo')
-    user_info = resp.json()
-    session['email'] = user_info['email']
-    return redirect('/index')
+    def get_frame(self):
+        """Return the current camera frame."""
+        BaseCamera.last_access = time.time()
 
+        # wait for a signal from the camera thread
+        BaseCamera.event.wait()
+        BaseCamera.event.clear()
 
-@app.route('/logout')
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('/index')
+        return BaseCamera.frame
 
+    @staticmethod
+    def frames():
+        """"Generator that returns frames from the camera."""
+        raise RuntimeError('Must be implemented by subclasses.')
 
-if __name__ == '__main__':
-    app.run(host='localhost', port='5000')
+    @classmethod
+    def _thread(cls):
+        """Camera background thread."""
+        print('Starting camera thread.')
+        frames_iterator = cls.frames()
+        for frame in frames_iterator:
+            BaseCamera.frame = frame
+            BaseCamera.event.set()  # send signal to clients
+            time.sleep(0)
+
+            # if there hasn't been any clients asking for frames in
+            # the last 10 seconds then stop the thread
+            if time.time() - BaseCamera.last_access > 10:
+                frames_iterator.close()
+                print('Stopping camera thread due to inactivity.')
+                break
+        BaseCamera.thread = None 
